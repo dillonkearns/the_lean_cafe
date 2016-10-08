a6d2ffd7ced4bd5fe37a39e024eb76465339c8ce
Dillon Kearns <dillon@dillonkearns.com>
2016-10-08 15:49:08 -0400

    Fix tests and remove unused controller actions

--- test/controllers/table_controller_test.exs
+++ test/controllers/table_controller_test.exs
@@ -21,14 +21,10 @@ defmodule TheLeanCafe.TableControllerTest do
     assert Repo.get_by(Table, @valid_attrs)
   end
 
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, table_path(conn, :create), table: @invalid_attrs
-    assert html_response(conn, 200) =~ "New table"
-  end
-
   test "shows chosen resource", %{conn: conn} do
     table = Repo.insert! %Table{}
-    conn = get conn, table_path(conn, :show, table)
+    %Table{id: table_id} = table
+    conn = get conn, table_path(conn, :show, Obfuscator.encode(table_id))
     assert html_response(conn, 200) =~ "Show table"
   end
 
@@ -38,29 +34,4 @@ defmodule TheLeanCafe.TableControllerTest do
     end
   end
 
-  test "renders form for editing chosen resource", %{conn: conn} do
-    table = Repo.insert! %Table{}
-    conn = get conn, table_path(conn, :edit, table)
-    assert html_response(conn, 200) =~ "Edit table"
-  end
-
-  test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    table = Repo.insert! %Table{}
-    conn = put conn, table_path(conn, :update, table), table: @valid_attrs
-    assert redirected_to(conn) == table_path(conn, :show, table)
-    assert Repo.get_by(Table, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    table = Repo.insert! %Table{}
-    conn = put conn, table_path(conn, :update, table), table: @invalid_attrs
-    assert html_response(conn, 200) =~ "Edit table"
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    table = Repo.insert! %Table{}
-    conn = delete conn, table_path(conn, :delete, table)
-    assert redirected_to(conn) == table_path(conn, :index)
-    refute Repo.get(Table, table.id)
-  end
 end
--- web/controllers/table_controller.ex
+++ web/controllers/table_controller.ex
@@ -36,35 +36,4 @@ defmodule TheLeanCafe.TableController do
     render(conn, "show.html", table: table)
   end
 
-  def edit(conn, %{"id" => id}) do
-    table = Repo.get!(Table, id)
-    changeset = Table.changeset(table)
-    render(conn, "edit.html", table: table, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "table" => table_params}) do
-    table = Repo.get!(Table, id)
-    changeset = Table.changeset(table, table_params)
-
-    case Repo.update(changeset) do
-      {:ok, table} ->
-        conn
-        |> put_flash(:info, "Table updated successfully.")
-        |> redirect(to: table_path(conn, :show, table))
-      {:error, changeset} ->
-        render(conn, "edit.html", table: table, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    table = Repo.get!(Table, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(table)
-
-    conn
-    |> put_flash(:info, "Table deleted successfully.")
-    |> redirect(to: table_path(conn, :index))
-  end
 end
