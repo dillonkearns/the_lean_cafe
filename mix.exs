1cdf809bafd367af052d5c7a94e37b673a65095f
Dillon Kearns <dillon@dillonkearns.com>
2016-10-07 22:41:36 -0400

    Add library for mapping table ids to secret urls

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule TheLeanCafe.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:hashids, "~> 2.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
--- web/views/table_view.ex
+++ web/views/table_view.ex
@@ -1,3 +1,12 @@
 defmodule TheLeanCafe.TableView do
   use TheLeanCafe.Web, :view
+
+  @salt Hashids.new([
+    salt: "123456",
+    min_len: 16,
+  ])
+
+  def obfuscate(%TheLeanCafe.Table{id: id}) do
+    Hashids.encode(@salt, id)
+  end
 end
