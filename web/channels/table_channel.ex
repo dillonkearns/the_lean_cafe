9b55e676ba384785962e378111ec2b8790efa594
Dillon Kearns <dillon@dillonkearns.com>
2016-10-09 16:36:50 -0400

    Limit channel messages within scope of current tables

--- web/channels/table_channel.ex
+++ web/channels/table_channel.ex
@@ -3,27 +3,29 @@ defmodule TheLeanCafe.TableChannel do
 
   intercept ["new_topic", "topics"]
 
-  def join(_room_name, params, socket) do
+  def join("table:" <> room_name, params, socket) do
     send(self, {:after_join, params})
     {:ok, socket}
   end
 
-  def topics do
+  def topics(table_hashid) do
+    table_id = Obfuscator.decode(table_hashid)
     table =
-      TheLeanCafe.Repo.get!(TheLeanCafe.Table, 1)
+      TheLeanCafe.Repo.get!(TheLeanCafe.Table, table_id)
       |> TheLeanCafe.Repo.preload(:topics)
     table.topics
     |> Enum.map(&(&1.name))
     |> Enum.reverse
   end
 
-  def handle_info({:after_join, _params}, socket) do
-    push socket, "topics", %{topics: topics}
+  def handle_info({:after_join, _params}, socket = %{topic: "table:" <> table_hashid}) do
+    push socket, "topics", %{topics: topics(table_hashid)}
     {:noreply, socket}
   end
 
-  def handle_in("new_topic", %{"body" => body}, socket) do
-    topic = %TheLeanCafe.Topic{table_id: 1, name: body}
+  def handle_in("new_topic", %{"body" => body}, socket = %{topic: "table:" <> table_hashid}) do
+    table_id = Obfuscator.decode(table_hashid)
+    topic = %TheLeanCafe.Topic{table_id: table_id, name: body}
     TheLeanCafe.Repo.insert!(topic)
     broadcast! socket, "new_topic", %{body: body}
     {:noreply, socket}
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -54,7 +54,7 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("table:asdf", {})
+let channel = socket.channel(`table:${roomHash()}`, {})
 let chatInput = document.querySelector("#topic-input")
 let messagesContainer = document.querySelector("#topics")
 
@@ -64,6 +64,10 @@ chatInput.addEventListener("keypress", event => {
     chatInput.value = ""
   }
 })
+
+function roomHash() {
+  return document.querySelector("#table-id").value
+}
 
 function addTopic(topicName) {
   let messageItem = document.createElement("li")
