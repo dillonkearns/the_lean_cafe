13309613b999d8d9e8e69a17fe3e10ac5c684a8c
Dillon Kearns <dillon@dillonkearns.com>
2016-10-09 10:42:32 -0400

    Use semaphore environment variable for database setup

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :the_lean_cafe, TheLeanCafe.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "postgres",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "postgres",
   database: "the_lean_cafe_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
