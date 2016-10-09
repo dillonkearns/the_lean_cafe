fe132284527f196a841597368ee1075093b4e811
Dillon Kearns <dillon@dillonkearns.com>
2016-10-09 12:15:10 -0400

    Update prod config file for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,21 @@ use Mix.Config
 # which you typically run after static files are built.
 config :the_lean_cafe, TheLeanCafe.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "theleancafe.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :the_lean_cafe, TheLeanCafe.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 
 # ## SSL Support
 #
