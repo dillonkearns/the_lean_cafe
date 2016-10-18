fa68b8d7c31cb7b07dd525e253af7587a899cfe6
Dillon Kearns <dillon@dillonkearns.com>
2016-10-18 11:01:15 -0700

    Add Topic.with_vote_counts function

--- test/models/topic_test.exs
+++ test/models/topic_test.exs
@@ -1,7 +1,7 @@
 defmodule TheLeanCafe.TopicTest do
   use TheLeanCafe.ModelCase
 
-  alias TheLeanCafe.Topic
+  alias TheLeanCafe.{Topic, Repo, DotVote}
 
   @valid_attrs %{name: "some content"}
   @invalid_attrs %{}
@@ -14,5 +14,19 @@ defmodule TheLeanCafe.TopicTest do
   test "changeset with invalid attributes" do
     changeset = Topic.changeset(%Topic{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "get list of tables with dot vote counts" do
+    table = Repo.insert!(%TheLeanCafe.Table{})
+    _topic1 = Repo.insert!(%Topic{table_id: table.id, name: "Unpopular Topic"})
+    topic2 = Repo.insert!(%Topic{table_id: table.id, name: "Popular Topic"})
+    topic3 = Repo.insert!(%Topic{table_id: table.id, name: "Regular Topic"})
+    Topic.vote_for!(topic2)
+    Topic.vote_for!(topic2)
+    Topic.vote_for!(topic2)
+    Topic.vote_for!(topic3)
+
+    topics_and_votes = Topic.with_vote_counts(table.id)
+    assert [{%Topic{name: "Unpopular Topic"}, 0}, {%Topic{name: "Popular Topic"}, 3}, {%Topic{name: "Regular Topic"}, 1}] = topics_and_votes
   end
 end
--- web/models/topic.ex
+++ web/models/topic.ex
@@ -1,5 +1,6 @@
 defmodule TheLeanCafe.Topic do
   use TheLeanCafe.Web, :model
+  alias TheLeanCafe.{Repo, Topic, DotVote}
 
   schema "topics" do
     field :name, :string
@@ -21,5 +22,19 @@ defmodule TheLeanCafe.Topic do
   def dot_vote_count(topic_id) do
     topic = TheLeanCafe.Repo.get(TheLeanCafe.Topic, topic_id) |> TheLeanCafe.Repo.preload(:dot_votes)
     length(topic.dot_votes)
+  end
+
+  def vote_for!(%Topic{id: id}) do
+    Repo.insert!(%DotVote{topic_id: id})
+  end
+
+  def with_vote_counts(table_id) do
+    query = from t in TheLeanCafe.Topic,
+      where: t.table_id == ^table_id,
+      order_by: t.id,
+      left_join: d in assoc(t, :dot_votes),
+      select: {t, count(d.id)},
+      group_by: t.id
+    TheLeanCafe.Repo.all(query)
   end
 end
