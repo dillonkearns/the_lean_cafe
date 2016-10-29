5c6ba0f27f81e8c15ee3d7ca4a68dd66f06d5e33
Dillon Kearns <dillon@dillonkearns.com>
2016-10-29 17:39:47 -0400

    Start writing new acceptance test

--- test/acceptance/add_topics_test.exs
+++ test/acceptance/add_topics_test.exs
@@ -10,6 +10,17 @@ defmodule TheLeanCafe.AddTopicsTest do
     assert visible_in_element?({:css, "#topics-incomplete li"}, ~r/Some interesting topic/)
   end
 
+  @tag :acceptance
+  test "add topics and vote" do
+    grab_a_table
+
+    add_topic("Something really boring")
+    add_topic("Something kind of interesting")
+    add_topic("Something really interesting")
+
+    assert visible_in_element?({:css, "#topics-incomplete li"}, ~r/Something really boring/)
+  end
+
   def add_topic(topic_name) do
     find_element(:css, "#topic-input")
     |> click
