da4590617430c781202108794cf4bd1197445a51
Dillon Kearns <dillon@dillonkearns.com>
2016-10-29 13:16:42 -0400

    Verify that topic appears in acceptance test

--- test/integration/add_topics_test.exs
+++ test/integration/add_topics_test.exs
@@ -6,5 +6,16 @@ defmodule TheLeanCafe.AddTopicsTest do
     navigate_to "/"
     find_element(:css, "[type=submit]")
     |> click
+
+    find_element(:css, "#topic-input")
+    |> click
+
+    send_text("Some interesting topic")
+
+    find_element(:css, ".topic-input-form [type=submit]")
+    |> click
+
+    assert visible_in_element?({:css, "#topics-incomplete li"}, ~r/Some interesting topic/)
   end
+
 end
